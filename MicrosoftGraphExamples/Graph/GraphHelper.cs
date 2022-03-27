7be4204b4a6ebcd74bcbd6615e7c7f7dc5848df9
Pj <petejw@theswamp.co.uk>
2022-03-27 17:36:37 +0100

    Add project files.

---
+++ MicrosoftGraphExamples/Graph/GraphHelper.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+
+namespace O365Api.Graph
+{
+    public class GraphHelper
+    {
+        public static GraphServiceClient GetGraphServiceClient(IConfiguration cfg)
+        {
+            // The client credentials flow requires that you request the
+            // /.default scope, and preconfigure your permissions on the
+            // app registration in Azure. An administrator must grant consent
+            // to those permissions beforehand.
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+            // https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+            return new GraphServiceClient(
+                GetTokenCredential(cfg), 
+                scopes);
+        }
+
+        private static TokenCredential GetTokenCredential(IConfiguration cfg)
+        {
+            // using Azure.Identity;
+            var options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            //var x = new DefaultAzureCredential()
+
+            return new ClientSecretCredential(
+                cfg["tenant"],
+                cfg["clientId"],
+                cfg["clientSecret"],
+                options);
+        }
+    }
+}
---
+++ MicrosoftGraphExamples/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using O365Api.Graph;
+using System;
+using System.Threading.Tasks;
+
+namespace O365Api
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cfg = LoadAppSettings();
+            var client = GraphHelper.GetGraphServiceClient(cfg);
+            
+            var users = await client.Users.Request()
+                //.Select(u => new
+                //{
+                //    u.Id,
+                //    u.DisplayName,
+                //    u.Messages,
+                //    u.JobTitle
+                //})
+                .GetAsync();
+
+
+            foreach(var user in users)
+            {
+                Console.WriteLine($"[{user.Id}] {user.DisplayName}");
+
+                //var u = await client.Users[user.Id].Request().GetAsync();
+
+                //Console.WriteLine($"[{u.Id}]");
+            }
+        }
+
+
+        static IConfigurationRoot LoadAppSettings()
+        {
+            return new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+        }
+    }
+}
